80e7040ca0e6602d409491d6964d385d2f6e8771
Aurélien <aurelien.strimelle@bstorm.be>
2023-04-28 17:20:14 +0200

    Exemple de persistance de store

--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,20 @@ import { configureStore } from '@reduxjs/toolkit';
 import presenceReducer from './reducers/presence.reducer';
 import itemReducer from './reducers/item.slice';
 
+// Best pratique pour la persistance du store :
+// Utiliser une librairie qui fera les modifications optimisé =)
 
 const store = configureStore({
   reducer: {
     presence: presenceReducer,
     item : itemReducer
   },
-  devTools: import.meta.env.dev
+  devTools: import.meta.env.dev, 
+  preloadedState: JSON.parse(localStorage.getItem('state') ?? '{}')
 });
+
+store.subscribe(() => {
+  localStorage.setItem('state',  JSON.stringify(store.getState()));
+})
 
 export default store;
